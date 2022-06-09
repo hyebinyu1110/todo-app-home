6de8eaa6dd0e8e29b7da61d048d099e578c3e06b
hyebinyu <hyebinyu1110@gmail.com>
2022-06-10 00:07:51 +0900

    repractice-7]

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from 'react-redux';
+import produce from 'immer';
 
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋값을 변경함
@@ -51,26 +52,27 @@ const initialState = {
 
 const todos = handleActions(
     {
-            [CHANGE_INPUT]: (state, {payload: input}) =>({
-                ...state,
-                input: input
-            }),
+            [CHANGE_INPUT]: (state, {payload: input}) =>
+               produce(state, draft=>{
+                   draft.input = input;
+               }),
+
                
-           [INSERT]: (state,{payload: todo}) =>({
-                    ...state,
-                    todos: state.todos.concat(todo),
+           [INSERT]: (state,{payload: todo}) =>
+                    produce(state, draft=>{
+                    draft.todos.push(todo);
                 }),
     
-            [TOGGLE]: (state, {payload: id}) =>({
-                    ...state,
-                    todos: state.todos.map(todo =>
-                        todo.id === id ? { ...todo, done: !todo.done } : todo)
+            [TOGGLE]: (state, {payload: id}) =>
+                    produce(state, draft =>{
+                    const todo = draft.todos.find(todo => todo.id === id);
+                    todo.done = !todo.done;
                 }),
     
-            [REMOVE]:(state, id) =>({
-                    ...state,
-                    todos: state.todos.filter(todo =>
-                        todo.id !== id)
+            [REMOVE]:(state, id) =>
+                   produce(state, draft=>{
+                    const index =  draft.todos.findIndex(todo => todo.id !== id);
+                    draft.todos.splice(index, 1);
                 }),
 
         },
