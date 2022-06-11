39d9b3b5a386f405398d9f202bad5effe45a6c79
hyebinyu <hyebinyu1110@gmail.com>
2022-06-11 16:54:03 +0900

    repractice-12

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,21 +1,18 @@
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from "../components/Todos";
+import useActions from '../lib/useActions';
 
 const TodosCounter = () => {
-    const { input, todos } = useSelector(state => ({
-        input: state.todos.input,
-        todos: state.todos.todos,
+    const { input, todos } = useSelector(({todos})=> ({
+        input: todos.input,
+        todos: todos.todos,
     }));
 
-    const dispatch = useDispatch();
-    const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
-    const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-    const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-    const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
-
+    const [onChangeInput,onInsert,onToggle,onRemove] = useActions(
+        [changeInput,insert,toggle,remove],
+        []
+    );
 
     return (
         <Todos
@@ -30,17 +27,4 @@ const TodosCounter = () => {
 };
 
 
-export default connect(
-    //비구조화 할당을 통해 todos를 분리하여 
-    // state.todos.input 대신 todos.input을 사용
-    ({ todos }) => ({
-        input: todos.input,
-        todos: todos.todos,
-    }),
-    {
-        changeInput,
-        insert,
-        toggle,
-        remove,
-    }
-)(TodosCounter);
+export default TodosCounter;
