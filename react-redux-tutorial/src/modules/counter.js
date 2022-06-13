b5d6c6694f009cdaacdfd0032dca7bf2c5fdd399
hyebinyu <hyebinyu1110@gmail.com>
2022-06-13 21:50:37 +0900

    repractice with redux middleware-1

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,23 +1,20 @@
-import {createAction, handleActions} from 'redux-actions';
+import { createAction, handleActions} from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-
-const initialState = {
-    number: 0
-}
+const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
 
 const counter = handleActions(
-    {
-      [INCREASE]: (state, action) =>({number: state.number + 1}),
-      [DECREASE]: (state, action) =>({number: state.number - 1}),
-    },
-    initialState,
+  {
+    [INCREASE]: state => state + 1,
+    [DECREASE]: state =>state -1
+
+  },
+  initialState
 );
 
 export default counter;
